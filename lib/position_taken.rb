7958f8dc17ef094e00fb417c170c598dadb642f4
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2020-12-31 20:14:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index].strip == "X" || board[index].strip == "O"
+  if board[index] == "X" || board[index] == "O"
     return true
   end
   return false
